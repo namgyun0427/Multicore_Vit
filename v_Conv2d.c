ebccae65cbb4878fabacaacbfcc0b08ace9394fb
hi0427 <david5330@naver.com>
2025-12-09 17:18:56 +0900

    Update v_Conv2d.c

--- v_Conv2d.c
+++ v_Conv2d.c
@@ -1,6 +1,5 @@
 #include "ViT_cl.h"
 
-
 void v_Conv2d(
     cl_mem m_input, cl_mem m_output,
     cl_mem m_weight, cl_mem m_bias
@@ -9,17 +8,16 @@ void v_Conv2d(
 
     cl_kernel k = container.kernels[__patch_embed];
 
-  
     int imgsize = IMG_SIZE;
     int outsize = OUTPUT_SIZE;
 
     KernelArg args[] = {
-        {.size = sizeof(cl_mem), .addr = &m_input },   // 0: Input
-        {.size = sizeof(cl_mem), .addr = &m_weight },  // 1: Weight
+        {.size = sizeof(cl_mem), .addr = &m_input },   // 0: Input Image
+        {.size = sizeof(cl_mem), .addr = &m_weight },  // 1: Weight (Filter)
         {.size = sizeof(cl_mem), .addr = &m_bias },    // 2: Bias
-        {.size = sizeof(cl_mem), .addr = &m_output },  // 3: Output
-        {.size = sizeof(int), .addr = &imgsize },      // 4: Image Size
-        {.size = sizeof(int), .addr = &outsize },      // 5: Output Grid Size
+        {.size = sizeof(cl_mem), .addr = &m_output },  // 3: Output Feature Map
+        {.size = sizeof(int), .addr = &imgsize },      // 4: Image Size (224)
+        {.size = sizeof(int), .addr = &outsize },      // 5: Output Grid Size (14)
     };
 
     for (int i = 0; i < 6; ++i) {
@@ -28,9 +26,9 @@ void v_Conv2d(
     }
 
     const size_t global_dim[] = {
-        (size_t)OUTPUT_SIZE,
-        (size_t)OUTPUT_SIZE,
-        (size_t)EMBED_DIM
+        (size_t)OUTPUT_SIZE,  // dim 0: ow
+        (size_t)OUTPUT_SIZE,  // dim 1: oh
+        (size_t)EMBED_DIM     // dim 2: oc (768)
     };
 
     err = clEnqueueNDRangeKernel(container.queue, k, 3, 0, global_dim, NULL, 0, NULL, NULL);
