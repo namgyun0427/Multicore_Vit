1596182eebfc10e5ef55c10c35ec458d2c53d579
hi0427 <david5330@naver.com>
2025-12-09 16:01:00 +0900

    Update v_Conv2d.c

--- v_Conv2d.c
+++ v_Conv2d.c
@@ -1,47 +1,54 @@
-#include "ViT_cl.h"
+__kernel void patch_embed(
+    __global const float* input,      // [C_in, H, W]
+    __global const float* weight,     // [C_out, C_in, K, K]
+    __global const float* bias,       // [C_out]
+    __global float* output,           // [C_out, Out_H, Out_W]
+    const int in_channels,            // 3
+    const int patch_size,             // 16
+    const int img_size,               
+    const int out_size                
+) {
+    const int ow = get_global_id(0); 
+    const int oh = get_global_id(1); 
+    const int oc = get_global_id(2); 
 
-// image patch embedding (convolution)
-// input[IN_CAHNS][PATCH_SIZE][PATCH_SIZE] => output[EMBED_DIM][n_patch_per_image][n_patch_per_image]
-void v_Conv2d(
-    cl_mem m_input, cl_mem m_output,
-    cl_mem m_weight, cl_mem m_bias
-    ) {
-    const int OUTPUT_SIZE = IMG_SIZE / PATCH_SIZE;
-
-    cl_kernel k = container.kernels[__patch_embed];
-
-    // set kernel args
-    // __kernel void patch_embed (
-    //     __global float* g_input,
-    //     __global float* g_weight,
-    //     __global float* g_bias,
-    //     __global float* g_output,
-    //     int IN_CAHNS,
-    //     int PATCH_SIZE,
-    //     int IMG_SIZE,
-    //     int OUTPUT_SIZE
-    // ) {
-    int in_chans = IN_CAHNS;
-    int patchsize = PATCH_SIZE;
-    int imgsize = IMG_SIZE;
-    int outsize = OUTPUT_SIZE;
-    KernelArg args[] = {
-        {.size = sizeof(cl_mem), .addr = &m_input },
-        {.size = sizeof(cl_mem), .addr = &m_weight },
-        {.size = sizeof(cl_mem), .addr = &m_bias },
-        {.size = sizeof(cl_mem), .addr = &m_output },
-        {.size = sizeof(int), .addr = &in_chans },
-        {.size = sizeof(int), .addr = &patchsize },
-        {.size = sizeof(int), .addr = &imgsize },
-        {.size = sizeof(int), .addr = &outsize },
-    };
-    for (int i = 0; i < 8; ++i) {
-        err = clSetKernelArg(k, i, args[i].size, args[i].addr);
-        CHECK_CL_ERROR(err);
+    if (ow >= out_size || oh >= out_size) {
+        return;
     }
 
-    // run kernel
-    const size_t global_dim[] = { OUTPUT_SIZE, OUTPUT_SIZE, EMBED_DIM };
-    err = clEnqueueNDRangeKernel(container.queue, k, 3, 0, global_dim, NULL, 0, NULL, NULL);
-    CHECK_CL_ERROR(err);
+    float sum = 0.0f;
+
+    const int in_start_x = ow * patch_size;
+    const int in_start_y = oh * patch_size;
+
+    for (int ic = 0; ic < in_channels; ++ic) {
+        
+        int img_offset = (ic * img_size * img_size) + (in_start_y * img_size) + in_start_x;
+
+        int weight_offset = (oc * in_channels * patch_size * patch_size) + (ic * patch_size * patch_size);
+     for (int kh = 0; kh < patch_size; ++kh) {
+            
+	    //float4 사용
+            for (int kw = 0; kw < patch_size; kw += 4) {
+                
+                float4 in_vec = vload4(0, input + img_offset + (kh * img_size) + kw);
+
+                float4 w_vec = vload4(0, weight + weight_offset + (kh * patch_size) + kw);
+
+                // sum += dot(in_vec, w_vec); 
+                sum = fma(in_vec.x, w_vec.x, sum);
+                sum = fma(in_vec.y, w_vec.y, sum);
+                sum = fma(in_vec.z, w_vec.z, sum);
+                sum = fma(in_vec.w, w_vec.w, sum);
+            }
+        }
+    }
+
+    //편향
+    sum += bias[oc];
+
+    // output [C_out, Out_H, Out_W]
+    // Index = (oc * Out_H * Out_W) + (oh * Out_W) + ow
+    int output_idx = (oc * out_size * out_size) + (oh * out_size) + ow;
+    output[output_idx] = sum;
 }
